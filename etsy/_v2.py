760f241fd9a304ca8b69883945de9e8fdb4fdb6d
Alek Storm <alek.storm@gmail.com>
2011-10-31 13:46:14 -0400

    Merge public and private API endpoints, update Sandbox endpoints to new subdomain

--- etsy/_v2.py
+++ etsy/_v2.py
@@ -8,16 +8,14 @@ except ImportError:
     from cgi import parse_qsl
 
 class EtsyV2(API):
-    api_url = 'http://openapi.etsy.com/v2/sandbox/public'
     api_version = 'v2'
 
     def __init__(self, api_key='', key_file=None, method_cache=missing, 
                  etsy_env=EtsyEnvSandbox(), log=None, etsy_oauth_client=None):
-        self.api_url = etsy_env.public_api_url
+        self.api_url = etsy_env.api_url
         self.etsy_oauth_client = None
 
         if etsy_oauth_client:
-            self.api_url = etsy_env.private_api_url
             self.etsy_oauth_client = etsy_oauth_client
 
         super(EtsyV2, self).__init__(api_key, key_file, method_cache, log)
--- etsy/etsy_env.py
+++ etsy/etsy_env.py
@@ -1,15 +1,11 @@
 class EtsyEnvSandbox(object):
-    request_token_url = 'http://openapi.etsy.com/v2/sandbox/oauth/request_token'
-    access_token_url = 'http://openapi.etsy.com/v2/sandbox/oauth/access_token'
+    request_token_url = 'http://sandbox.openapi.etsy.com/v2/oauth/request_token'
+    access_token_url = 'http://sandbox.openapi.etsy.com/v2/oauth/access_token'
     signin_url = 'https://www.etsy.com/oauth/signin'
-    public_api_url = 'http://openapi.etsy.com/v2/sandbox/public'
-    private_api_url = 'http://openapi.etsy.com/v2/sandbox/private'
+    api_url = 'http://sandbox.openapi.etsy.com/v2'
 
 class EtsyEnvProduction(object):
     request_token_url = 'http://openapi.etsy.com/v2/oauth/request_token'
     access_token_url = 'http://openapi.etsy.com/v2/oauth/access_token'
     signin_url = 'https://www.etsy.com/oauth/signin'
-    public_api_url = 'http://openapi.etsy.com/v2/public'
-    private_api_url = 'http://openapi.etsy.com/v2/private'
-
-
+    api_url = 'http://openapi.etsy.com/v2'
